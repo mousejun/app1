2d4f65ad4b0c3ebcaf4e445e12b027e22505c095
mousejun <jooa2009@gmail.com>
2025-11-14 21:58:26 +0900

    Delete app directory

--- app/src/main/java/com/community/app_1113/BookmarkItem.kt
+++
@@ -1,7 +0,0 @@
-package com.community.app_1113
-
-data class BookmarkItem(
-    val name: String,
-    val url: String,
-    val category: String = ""
-)
--- app/src/main/java/com/community/app_1113/PageHistoryItem.kt
+++
@@ -1,8 +0,0 @@
-package com.community.app_1113
-
-data class PageHistoryItem(
-    val title: String,
-    val url: String,
-    val timestamp: Long = System.currentTimeMillis()
-)
-
